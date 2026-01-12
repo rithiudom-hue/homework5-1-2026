9da37f551756a1c3e2d926023420193a9ed02f95
unknown <suthsavuth@gmail.com>
2026-01-12 10:04:16 +0700

    setup base project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+//    ti1
+    public static void main(String[] args) {
+//        System.out.println("my name : saroeun rithiudom");
+//        System.out.println("age : 17 year old");
+//        System.out.println("Siem Reap");
+//    ti2
+      Scanner sc = new Scanner(System.in);
+
+      System.out.print("Please input name: ");
+      String studentName = sc.next();
+
+      System.out.print("Please input Age: ");
+      String studentAge =sc.next();
+
+      System.out.print("please input height: ");
+      String studentHeight = sc.next();
+
+      System.out.print("Please input Place is student: ");
+      boolean placeIsStudent = sc.hasNextBoolean();
+
+      System.out.println("Student Name = " +studentName);
+      System.out.println("Student Age = " +studentAge);
+      System.out.println("Student Height = " +studentHeight);
+      System.out.println("Student place is = " +placeIsStudent);
+
+    }
+}
---
+++ src/main/java/org/example/varabel.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class varabel {
+    public static void main(String[] args) {
+//        ti1
+//        int absent = 0;
+//        double average = 30.50;
+//
+//        if (average >25 && absent <=10) {
+//            System.out.println("Pass ");
+//            System.out.println("Congratulation!!!!!");
+//        }else {
+//            System.out.println("Fail");
+//            System.out.println("Try again next year.");
+//        }
+//        if (average<25 || absent >10){
+//            System.out .println("Fail");
+//        }
+//        ti2
+
+        int number = 2023;
+        if (Number % 2 == 0){
+            System.out.println("Even number");
+
+        }else {
+            System.out.println("Odd number");
+        }
+    }
+}
